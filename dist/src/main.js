a26a98ea928276f74e711b93d17a87e45ffeb16e
Rodolfo Parada <rodolfo.parada.gonzalez>
2026-05-22 15:40:49 -0400

    fix: cors ssl y usuaio demo

--- dist/src/main.js
+++ dist/src/main.js
@@ -3,6 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const swagger_1 = require("@nestjs/swagger");
 const app_module_1 = require("./app.module");
+function normalizeOrigin(url) {
+    return url.endsWith('/') ? url.slice(0, -1) : url;
+}
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const config = new swagger_1.DocumentBuilder()
@@ -13,9 +16,23 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api/docs', app, document);
+    const frontendUrl = process.env.FRONTEND_URL
+        ? normalizeOrigin(process.env.FRONTEND_URL)
+        : undefined;
+    const allowedOrigins = [
+        'http://localhost:5173',
+        'http://localhost:3000',
+    ];
+    if (frontendUrl) {
+        allowedOrigins.push(frontendUrl);
+        const withoutHttps = frontendUrl.replace(/^https?:\/\//, '');
+        allowedOrigins.push(new RegExp(`^https?://${withoutHttps.replace(/\./g, '\\.')}/?$`));
+    }
     app.enableCors({
-        origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+        origin: allowedOrigins,
         credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
     });
     await app.listen(process.env.PORT ?? 3000);
 }
